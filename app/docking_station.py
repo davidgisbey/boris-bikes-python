f328824047e24a02ec498935ae8ccc45c347ba03
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-12-17 14:39:13 +0000

    Add Bike#release
    Adds basic error handling if bike isn't present in the bikes array.

--- app/docking_station.py
+++ app/docking_station.py
@@ -1,3 +1,10 @@
 class DockingStation:
   def __init__(self, bikes = []):
     self.bikes = bikes
+
+  def release_bike(self, bike):
+    if bike not in self.bikes:
+      return "Bike not in docking station"
+
+    self.bikes.remove(bike)
+    return bike
--- tests/docking_station_test.py
+++ tests/docking_station_test.py
@@ -1,5 +1,6 @@
 import pytest
 from app.docking_station import DockingStation
+from app.bike import Bike
 
 def test_docking_station_initialises_with_empty_array_for_bikes():
   docking_station = DockingStation()
@@ -9,3 +10,21 @@ def test_pass_bikes_in_on_initialisation():
   docking_station = DockingStation(['bike1', 'bike2'])
   assert docking_station.bikes == ['bike1', 'bike2']
 
+def test_can_release_bike():
+  bike1 = Bike()
+  bike2 = Bike()
+  bike3 = Bike()
+  docking_station = DockingStation([bike1, bike2, bike3])
+
+  released_bike1 = docking_station.release_bike(bike1)
+  released_bike2 = docking_station.release_bike(bike3)
+
+  assert released_bike1 == bike1
+  assert released_bike2 == bike3
+  assert docking_station.bikes == [bike2]
+
+def test_bike_not_in_docking_station():
+  bike1 = Bike()
+  docking_station = DockingStation()
+
+  assert docking_station.release_bike(bike1) == "Bike not in docking station"
